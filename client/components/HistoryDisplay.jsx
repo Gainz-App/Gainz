37b274d06be1b3cbd07743a3c9907c38ceea106f
PLCoster <58193305+PLCoster@users.noreply.github.com>
2021-08-28 14:22:30 -0700

    Added basic react component templates for site with React Router Links
    Co-authored-by: Lindsay Bird <lindsay.a.baird@gmail.com>

---
+++ client/components/HistoryDisplay.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HistoryDisplay = () => {
+  return(
+    <div>
+      <h1>History Goes Here:</h1>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+      </ul>
+    </div>
+  );
+};
+
+export default HistoryDisplay;
