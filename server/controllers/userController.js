39f44a22adc5500668cea1926878d6b0ddc6e396
Edwin Lee <edjl1289@gmail.com>
2021-08-29 16:06:05 -0700

    committed /setup fetch request to connect FE BE and exercisesDisplay

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,8 +3,8 @@ const db = require('../postgresPool');
 const userController = {};
 
 userController.newUser = (req, res, next) => {
-console.log('THIS IS TO CREATE NEW USERS ');
-const newUsersQ = 'INSERT INTO users (_id, name, email, password) VALUES ("234", "Edwin", "edwinl@gmail.com", "987")';
+  console.log('THIS IS TO CREATE NEW USERS ');
+  const newUsersQ = 'INSERT INTO users (_id, name, email, password) VALUES ("234", "Edwin", "edwinl@gmail.com", "987")';
   db.query(newUsersQ)
     .then((data) => {
       res.locals.newUsersQuery = data.rows;
