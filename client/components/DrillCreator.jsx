9bcc2e98152c55a9aaff8f82a312f9f1350f4e13
Lindsay Baird <lindsay.a.baird@gmail.com>
2021-08-29 19:12:09 -0700

    Updated a little more Drill Creator, ExerciseCreator, ExercisesDisplay Authored By: Lindsay Baird <lindsay.a.baird@gmail.com>

--- client/components/DrillCreator.jsx
+++ client/components/DrillCreator.jsx
@@ -6,10 +6,13 @@ const DrillCreator = () => {
   const [drillData, setDrillData] = useState({});
 
   // TODO MAKE REAL API CALL OR LIFT STATE TO APP
+  //Is there a route for creating a drill? I only see createExercise
   const getExercise = () => {
     setDrillData(
+      // How to we enter type_id here? The type should just be type, selected from the types table.Then I suppose we have to give the type_id the correct number based on user input
+      // How do we use the id? Don't they need to put it in somewhere? 
       {
-        name: id, description: '???', type: '???', last_weight: '???', last_reps: '???', last_sets: '???',
+        name: id, description: 'super tough', type: 'legs', init_weight: 300, init_reps: 10, init_sets: 3,
       },
     );
   };
@@ -18,6 +21,14 @@ const DrillCreator = () => {
     console.log('Getting data from server for drill');
     getExercise();
   }, []);
+
+  const handleSubmitClick = () => {
+    // click submit, drillData goes to DB, return to root
+  };
+
+  const handleCancelClick = () => {
+    // return to root
+  };
 
   return (
     <div className='drill'>
@@ -30,6 +41,22 @@ const DrillCreator = () => {
         <ul>{drillData.last_reps}</ul>
         <ul>{drillData.last_sets}</ul>
       </li>
+      <form>
+        <p>Enter Today's Weight:</p>
+        <input 
+          type="text"
+          name="weight"
+          value=""
+         />
+        <p>Enter Today's Sets:</p>
+        <input type="text" name="sets" value="" />
+        <p>Enter Today's Reps</p>
+        <input type="text" name="reps" value="" />
+        <p>Enter Today's Rest</p>
+        <input type="text" name="rest" value="" />
+        <button onClick={handleSubmitClick}>Submit< /button>
+        <button onClick={handleCancelClick}>Cancel< /button>
+      </form>
     </div>
 
   );
